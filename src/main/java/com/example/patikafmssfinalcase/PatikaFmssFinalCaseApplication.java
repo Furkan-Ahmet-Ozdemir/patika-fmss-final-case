51bf2cd54c651551970f4d7f43ea0b7bbe4572e5
Furkan-Ahmet-Ozdemir <furkanahmetozdemir@hotmail.com>
2024-05-18 00:16:47 +0300

    Initial commit

---
+++ src/main/java/com/example/patikafmssfinalcase/PatikaFmssFinalCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.patikafmssfinalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaFmssFinalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaFmssFinalCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/patikafmssfinalcase/PatikaFmssFinalCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patikafmssfinalcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaFmssFinalCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
