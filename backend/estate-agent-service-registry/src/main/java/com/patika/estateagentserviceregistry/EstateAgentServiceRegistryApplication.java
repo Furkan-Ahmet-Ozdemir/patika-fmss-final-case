25999459f14b2f91f0a6c7f2dc09fc5c9f068c7c
Furkan-Ahmet-Ozdemir <furkanahmetozdemir@hotmail.com>
2024-07-02 16:52:57 +0300

    service registry added

---
+++ backend/estate-agent-service-registry/src/main/java/com/patika/estateagentserviceregistry/EstateAgentServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.patika.estateagentserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateAgentServiceRegistryApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(EstateAgentServiceRegistryApplication.class, args);
+    }
+
+}
---
+++ backend/estate-agent-service-registry/src/test/java/com/patika/estateagentserviceregistry/EstateAgentServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.estateagentserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateAgentServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+
+}
