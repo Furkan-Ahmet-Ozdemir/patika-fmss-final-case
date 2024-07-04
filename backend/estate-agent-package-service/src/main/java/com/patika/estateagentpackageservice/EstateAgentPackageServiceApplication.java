063c2c355cbcbb3a31f5003a4b4dc2447230fd9d
Furkan-Ahmet-Ozdemir <furkanahmetozdemir@hotmail.com>
2024-07-04 13:18:39 +0300

    package service added

---
+++ backend/estate-agent-package-service/src/main/java/com/patika/estateagentpackageservice/EstateAgentPackageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patika.estateagentpackageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateAgentPackageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateAgentPackageServiceApplication.class, args);
+	}
+
+}
